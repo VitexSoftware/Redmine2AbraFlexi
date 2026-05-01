56d607614643eac53681e8f71eebc922753724a6
CyberVitexus <info@vitexsoftware.cz>
2026-05-01 18:33:07 +0200

    feat: add localization files and update autoloader syntax

--- debian/autoload.php
+++ debian/autoload.php
@@ -5,19 +5,19 @@ require_once '/usr/share/php/Composer/InstalledVersions.php';
 require_once '/usr/share/php/AbraFlexi/autoload.php';
 
 // PSR-4 autoloader for application classes
-spl_autoload_register(function (string $$class): void {
-    $$prefixes = [
+spl_autoload_register(function (string $class): void {
+    $prefixes = [
         'Redmine2AbraFlexi\\' => '/usr/share/redmine2abraflexi/src/Redmine2AbraFlexi/',
     ];
-    foreach ($$prefixes as $$prefix => $$baseDir) {
-        $$len = strlen($$prefix);
-        if (strncmp($$prefix, $$class, $$len) !== 0) {
+    foreach ($prefixes as $prefix => $baseDir) {
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
             continue;
         }
-        $$relativeClass = substr($$class, $$len);
-        $$file = $$baseDir . str_replace('\\', '/', $$relativeClass) . '.php';
-        if (file_exists($$file)) {
-            require $$file;
+        $relativeClass = substr($class, $len);
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+        if (file_exists($file)) {
+            require $file;
             return;
         }
     }

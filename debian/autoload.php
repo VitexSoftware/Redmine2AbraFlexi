5d85490fe18e7bdb1eec8a2a3bcfafffdc9a22be
CyberVitexus <info@vitexsoftware.cz>
2026-05-01 17:37:18 +0200

    feat: add internationalization support and update debian packaging for composer integration

--- debian/autoload.php
+++ debian/autoload.php
@@ -1,6 +1,7 @@
 <?php
 // Debian autoloader for redmine2abraflexi
 // Load dependency autoloaders
+require_once '/usr/share/php/Composer/InstalledVersions.php';
 require_once '/usr/share/php/AbraFlexi/autoload.php';
 
 // PSR-4 autoloader for application classes
@@ -21,3 +22,21 @@ spl_autoload_register(function (string $$class): void {
         }
     }
 });
+
+(function (): void {
+    $versions = [];
+    foreach (\Composer\InstalledVersions::getAllRawData() as $d) {
+        $versions = array_merge($versions, $d['versions'] ?? []);
+    }
+    $name    = 'unknown';
+    $version = '0.0.0';
+    $versions[$name] = ['pretty_version' => $version, 'version' => $version,
+        'reference' => null, 'type' => 'project', 'install_path' => __DIR__,
+        'aliases' => [], 'dev_requirement' => false];
+    \Composer\InstalledVersions::reload([
+        'root' => ['name' => $name, 'pretty_version' => $version, 'version' => $version,
+            'reference' => null, 'type' => 'project', 'install_path' => __DIR__,
+            'aliases' => [], 'dev' => false],
+        'versions' => $versions,
+    ]);
+})();
